f4c25b9a9a5d221efea0ef11d5b599a90075e47c
IdinaldoTeteo <idinaldoteteo@gmail.com>
2022-04-25 08:56:53 -0300

    Create Project UsuarioApi

---
+++ UsuarioApi/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuarioApi.Data.Dtos;
+
+namespace UsuarioApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        
+        [HttpPost]
+        public IActionResult CadastrarUsuario(CreateUsuarioDto usuarioDto)
+        {
+
+            return Ok(usuarioDto);
+        }
+    }
+}
