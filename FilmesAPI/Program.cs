c1e9497b46bedbecab46d0e1056fa7f5c37fe680
IdinaldoTeteo <idinaldoteteo@gmail.com>
2022-05-28 10:48:54 -0300

    add: Serilog

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FilmesAPI
 {
@@ -13,11 +10,28 @@ namespace FilmesAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                           .Enrich.FromLogContext()
+                           .WriteTo.Console()
+                           .CreateLogger();
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Erro na FilmesAPI");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
---
+++ UsuarioApi/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsuarioApi.Data
+{
+    public class UserDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
